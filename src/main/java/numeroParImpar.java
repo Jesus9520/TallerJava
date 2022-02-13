97f28a101f962816088f7e2b21feeeabf20ad5e5
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 18:45:55 -0500

    Ejercicio 6, usando el ciclo for, numeros pares e impares

---
+++ src/main/java/numeroParImpar.java
@@ -0,0 +1,23 @@
+public class numeroParImpar {
+
+    public static void main(String[] args) {
+        String numPar="",numImpar="";
+
+        for (int i=1;i<=100;i++){
+            if(i%2==0){
+                numPar=numPar +String.valueOf(i)+"-";
+
+            }else{
+                numImpar=numImpar +String.valueOf(i)+"-";
+
+            }
+        }
+
+        System.out.println("numeros pares:");
+        System.out.println(numPar);
+
+        System.out.println("numeros impares:");
+        System.out.println(numImpar);
+    }
+}
+
