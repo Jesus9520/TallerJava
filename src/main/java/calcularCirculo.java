4e6a877dec48ea0881228f3ae3fe5b54e4660a34
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 18:23:15 -0500

    Tercer commit para el ejercicio 3

---
+++ src/main/java/calcularCirculo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+public class calcularCirculo {
+
+    private static boolean isNumeric(String cadena) {
+        try {
+            Double.parseDouble(cadena);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String radio;
+
+        do {
+            System.out.println("ingrese el radio del circulo");
+            Scanner imprimirCirculo = new Scanner(System.in);
+            radio = imprimirCirculo.next();
+        } while (!isNumeric(radio));
+
+        double r = Double.parseDouble(radio);
+        double area = PI * pow(r, 2);
+
+        System.out.println("el area del circulo es: " + area);
+
+    }
+
+
+}
