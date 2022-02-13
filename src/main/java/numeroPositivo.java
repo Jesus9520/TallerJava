3f37fb35b5ca3279c6f9b48c8d4bbf424711deff
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 18:55:46 -0500

    Ejercicio 7, leer por teclado y comprobar si es mayor o igual a 0

---
+++ src/main/java/numeroPositivo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class numeroPositivo {
+    //método que valida si una cadena es numerica
+    public static boolean isNumeric(String cadena) {
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+    // método de tipo main que lee de teclado un numero y verifica si es mayor o igual a 0
+    public static void main(String[] args) {
+        String num;
+        do {
+            System.out.println("ingrese un número");
+            Scanner imprimirNumero = new Scanner(System.in);
+            num = imprimirNumero.next();
+        }while (!isNumeric(num)||Integer.parseInt(num)<0);
+
+        System.out.println("el número ingresado fue: " + num);
+    }
+}
