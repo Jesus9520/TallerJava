10b87a6ca2b09b9702f0f9d80b84efe29edffe0f
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 18:33:52 -0500

    Calcular el precio del articulo más iva, Ejercicio 4

---
+++ src/main/java/precioProducto.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
+public class precioProducto {
+    private static boolean isNumeric(String cadena){
+        try {
+            Double.parseDouble(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        String precioArticulo;
+
+        do {
+            System.out.println("ingrese el precio del articulo");
+            Scanner imprimirPrecio = new Scanner(System.in);
+            precioArticulo = imprimirPrecio.next();
+        }while(!isNumeric(precioArticulo));
+
+        double r=Double.parseDouble(precioArticulo);
+        double precioArtIva= r+(r*0.21);
+
+        System.out.println("el precio final del articulo es: "+ precioArtIva);
+    }
+}
