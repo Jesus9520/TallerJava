4145dd9123d9a7ec188d5ccd3ac2251ccdc9651c
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 17:29:46 -0500

    Primer commit del taller de Java, compararNumeros

---
+++ src/main/java/compararNumeros.java
@@ -0,0 +1,17 @@
+public class compararNumeros {
+    public static void main(String[] args) {
+        int num_1 = 3;
+        int num_2 = 5;
+
+        if(num_2>num_1){
+            System.out.println("el primer número es mayor que el segundo");
+        }else if (num_1>num_2){
+            System.out.println("el segundo número es mayor que el primero");
+        }else{
+            System.out.println("los dos números son iguales");
+        }
+    }
+}
+
+
+
