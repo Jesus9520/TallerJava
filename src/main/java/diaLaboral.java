c94fbf78ea0c18ce93abc4367126557041ebb905
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 19:00:40 -0500

    Ejercicio 8, Dia laboral

---
+++ src/main/java/diaLaboral.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class diaLaboral {
+    public static void main(String[] args) {
+        System.out.println("ingrese un día de la semana");
+        Scanner imprimirDiaLaboral = new Scanner(System.in);
+        String dia = imprimirDiaLaboral.next();
+        dia = dia.toLowerCase();
+        switch (dia){
+
+            case "lunes":
+                System.out.println("Lunes es un día laboral");
+                break;
+            case "martes":
+                System.out.println("Martes es un día laboral");
+                break;
+            case "miercoles":
+                System.out.println("Miercoles es un día laboral");
+                break;
+            case "jueves":
+                System.out.println("Jueves es un día laboral");
+                break;
+            case "viernes":
+                System.out.println("Viernes es un día laboral");
+                break;
+            case "sabado":
+                System.out.println("Sabado no es un día laboral");
+                break;
+            case "domingo":
+                System.out.println("Domingo no es un día laboral");
+                break;
+            default:
+                System.out.println("ingrese un día de la semana correctamente");
+
+        }
+    }
+}
